56d6101a168db7c4af0d1d8e133c4735ffa187f6
silva <silva@cfsilva.com>
2023-06-01 12:01:55 +0200

    styles

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -13,32 +13,39 @@
 <div class="container">
 	<h1>Register</h1>
 	<form method="POST" use:enhance>
-		<label>
+		<label for="username">
 			Username
-			<input name="username" type="text" id="username" data-invalid={$errors.username} bind:value={$form.username} {...$constraints.username} />
-			{#if $errors.username}<span class="invalid">{$errors.username}</span>{/if}
 		</label>
-		<label>
+		<input name="username" type="text" id="username" data-invalid={$errors.username} bind:value={$form.username} {...$constraints.username} />
+		{#if $errors.username}<span class="invalid">{$errors.username}</span>{/if}
+		<label for="email">
 			Email
-			<input name="email" type="email" id="email" data-invalid={$errors.email} bind:value={$form.email} {...$constraints.email}/>
-			{#if $errors.email}<span class="invalid">{$errors.email}</span>{/if}
 		</label>
-		<label>
+		<input name="email" type="email" id="email" data-invalid={$errors.email} bind:value={$form.email} {...$constraints.email}/>
+		{#if $errors.email}<span class="invalid">{$errors.email}</span>{/if}
+		<label for="password">
 			Password
-			<input name="password" type="password" id="password" data-invalid={$errors.password} bind:value={$form.password} {...$constraints.password} />
-			{#if $errors.password}<span class="invalid">{$errors.password}</span>{/if}
 		</label>
-		<label>
+		<input name="password" type="password" id="password" data-invalid={$errors.password} bind:value={$form.password} {...$constraints.password} />
+		{#if $errors.password}<span class="invalid">{$errors.password}</span>{/if}
+		<label for="confirmPassword">
 			Confirm Password
-			<input name="confirmPassword" type="password" id="confirmPassword" data-invalid={$errors.confirmPassword} bind:value={$form.confirmPassword} {...$constraints.confirmPassword} />
-			{#if $errors.confirmPassword}<span class="invalid">{$errors.confirmPassword}</span>{/if}
 		</label>
+		<input name="confirmPassword" type="password" id="confirmPassword" data-invalid={$errors.confirmPassword} bind:value={$form.confirmPassword} {...$constraints.confirmPassword} />
+		{#if $errors.confirmPassword}<span class="invalid">{$errors.confirmPassword}</span>{/if}
 		<button>Register{#if $delayed}<span class="delayed"><i class="fa-solid fa-gear fa-spin"></i></span>{/if}</button>
 	</form>
 </div>
 
 <style>
 	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	form {
 		display: flex;
 		justify-content: center;
 		align-items: center;
