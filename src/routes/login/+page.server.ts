fa257431108ccc1380525a3976eddb248d44df34
silva <silva@cfsilva.com>
2023-06-01 14:51:56 +0200

    Initial Login config

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,61 @@
+import { prisma } from '../../lib/server/prisma';
+import { fail, redirect } from '@sveltejs/kit';
+import { z } from 'zod';
+import { superValidate, setError } from 'sveltekit-superforms/server';
+import type { Actions } from '@sveltejs/kit';
+import bcrypt from 'bcrypt';
+
+const loginSchema = z.object({
+	username: z.string(),
+	password: z.string().min(5)
+});
+
+export const load = async () => {
+	// Server API:
+	const form = await superValidate(loginSchema);
+
+	//We need to always return form in Superforms
+	return { form };
+};
+
+export const actions = {
+	//named default because there's just one
+	default: async ({ request }) => {
+		//validate form using Zod
+		const form = await superValidate(request, loginSchema);
+		//return the error
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		//get data from form
+		const { username, password } = form.data as {
+			username: string;
+			password: string;
+		};
+
+		//fetch user from the DB
+		const existingUser = await prisma.user.findUnique({
+			where: {
+				username: username
+			}
+		});
+
+		//if user exists, compare passwords, if not -> error
+		if (existingUser) {
+			//bcrypt's password check
+			const passwordCheck = bcrypt.compareSync(password, existingUser.encryptedPassword);
+			//if it returns true, redirect user, if not -> error
+			if (passwordCheck) {
+				throw redirect(301, '/dashboard');
+			} else {
+				return setError(form, 'password', 'Incorrect password.');
+			}
+		} else {
+			return setError(form, 'username', 'This username does not exist.');
+		}
+
+		//return form for Superform
+		return { form };
+	}
+} satisfies Actions;
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	//superforms
+	import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte';
+	import { superForm } from 'sveltekit-superforms/client'
+	export let data;
+
+	// superform API:
+	const { form, enhance, errors, constraints, delayed  } = superForm(data.form, {
+		multipleSubmits: 'prevent'
+	});
+
+</script>
+<div class="container">
+	<h1>Login</h1>
+	<form method="POST" use:enhance>
+		<label for="username">
+			Username
+		</label>
+		<input name="username" type="text" id="username" data-invalid={$errors.username} bind:value={$form.username} {...$constraints.username} />
+		{#if $errors.username}<span class="invalid">{$errors.username}</span>{/if}
+		<label for="password">
+			Password
+		</label>
+		<input name="password" type="password" id="password" data-invalid={$errors.password} bind:value={$form.password} {...$constraints.password} />
+		{#if $errors.password}<span class="invalid">{$errors.password}</span>{/if}
+		<button>Login{#if $delayed}<span class="delayed"><i class="fa-solid fa-gear fa-spin"></i></span>{/if}</button>
+	</form>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	form {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	.invalid {
+		color: red;
+	}
+</style>
