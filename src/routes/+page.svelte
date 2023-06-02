5c794a4ec173d3d19f28427fdec938637a5e8fb8
silva <silva.cesario49@gmail.com>
2023-06-02 14:14:10 +0200

    Added login/register styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
-<h1>Main page</h1>
-<a href='/register'>Register</a>
-<a href="/login">Login</a>
+<div class="flex items-center justify-center h-screen flex-col">
+	<h1 class="h1 m-10">Welcome</h1>
+	<div class="flex gap-8">
+		<form action="/login">
+			<button class="btn variant-filled-primary">Login</button>
+		</form>
+		<form action="/register">
+			<button class="btn variant-filled-primary">Register</button>
+		</form>
+	</div>
+</div>
