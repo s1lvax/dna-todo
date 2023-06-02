409894fb811bb6b4dff9f3b24640857ca63ccd15
silva <silva.cesario49@gmail.com>
2023-06-02 09:58:28 +0200

    fixed layout.server.ts

--- src/routes/dashboard/+layout.server.ts
+++ src/routes/dashboard/+layout.server.ts
@@ -1,6 +1,7 @@
 import type { LayoutServerLoad } from './$types';
 import { prisma } from '$lib/server/prisma';
 import { z } from 'zod';
+import { redirect } from '@sveltejs/kit';
 
 const todoSchema = z.object({
 	title: z.string().min(3).max(15),
@@ -8,6 +9,10 @@ const todoSchema = z.object({
 });
 
 export const load: LayoutServerLoad = async ({ locals }) => {
+	//redirect users if not logged in
+	if (!locals.user) {
+		throw redirect(302, '/login');
+	}
 	//get existing tasks
 	const existingTasks = await prisma.todo.findMany({
 		where: { authorId: locals.user.userid },
